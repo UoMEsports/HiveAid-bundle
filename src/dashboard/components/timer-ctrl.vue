9d61a5d9c125f3be0ccf2d95ee5c4c26fff756b6
Joshua Fillingham <j.j.fillingham@edu.salford.ac.uk>
2019-02-25 02:36:25 +0000

    chore: changed spacing in timer-ctrl.vue

--- src/dashboard/components/timer-ctrl.vue
+++ src/dashboard/components/timer-ctrl.vue
@@ -1,15 +1,15 @@
 <template>
-	<div>
-		<h1 class="time">{{time}}</h1>
-		<h2 class="nextTime">{{nextTime}}</h2>
-		<h1 class="text">{{text}}</h1>
-		<v-btn @click="toggleTimer" :color="startStopColour">{{startStopContent}}</v-btn>
-		<v-btn @click="toggleHidden" :color="showHideColour">{{showHideContent}}</v-btn>
-		<v-btn color="orange" nodecg-dialog="timer-dialog-to">Run to</v-btn>
-		<v-btn color="orange" nodecg-dialog="timer-dialog-for">Run for</v-btn>
-		<nodecg-text v-model="newText" dark replicantName="countdownNewText"></nodecg-text>
-		<v-btn @click="setText" color="green">Update Text</v-btn>
-	</div>
+  <div>
+    <h1 class="time">{{ time }}</h1>
+    <h2 class="nextTime">{{ nextTime }}</h2>
+    <h1 class="text">{{ text }}</h1>
+    <v-btn @click="toggleTimer" :color="startStopColour">{{ startStopContent }}</v-btn>
+    <v-btn @click="toggleHidden" :color="showHideColour">{{ showHideContent }}</v-btn>
+    <v-btn color="orange" nodecg-dialog="timer-dialog-to">Run to</v-btn>
+    <v-btn color="orange" nodecg-dialog="timer-dialog-for">Run for</v-btn>
+    <nodecg-text v-model="newText" dark replicantName="countdownNewText"></nodecg-text>
+    <v-btn @click="setText" color="green">Update Text</v-btn>
+  </div>
 </template>
 
 <script>
@@ -19,74 +19,74 @@ const countdownNext = nodecg.Replicant('countdownNext');
 const TimeUtils = require('../../../extension/lib/time');
 const countdownText = nodecg.Replicant('countdownText');
 export default {
-	data() {
-		return {
-			time: "0:00",
-			running: false,
-			hidden: false,
-			text: 'UP NEXT',
-			newText: '',
-			nextTime: null
-		};
-	},
-	created() {
-		NodeCG.waitForReplicants(countdownRep).then(this.listen);
-	},
-	methods: {
-		toggleTimer() {
-			if (running.value) {
-				nodecg.sendMessage('stopCountdown', countdownNext.value);
-			} else {
-				nodecg.sendMessage('startCountdown', countdownNext.value);
-			}
-		},
-		toggleHidden() {
-			nodecg.sendMessage('showHideTimer');
-		},
-		setText() {
-			countdownText.value = this.newText;
-		},
-		listen() {
-			this.newText = countdownText.value;
-			countdownRep.on('change', newVal => {
-				this.time = newVal.formatted;
-				this.hidden = newVal.hidden;
-			});
-			running.on('change', newVal => {
-				this.running = newVal;
-			});
-			countdownText.on('change', newVal => {
-				this.text = newVal;
-			});	
-			countdownNext.on('change', newVal => {
-				let formattedNextTime = TimeUtils.createTimeStruct(newVal * 1000);
-				this.nextTime = formattedNextTime.formatted;
-			});
-		}
-	},
-	computed: {
-		startStopContent() {
-			return this.running ? 'STOP' : 'START';
-		},
-		startStopColour() {
-			return this.running ? 'red' : 'green';
-		},
-		showHideContent() {
-			return this.hidden ? 'SHOW' : 'HIDE';
-		},
-		showHideColour() {
-			return !this.hidden ? 'red' : 'green';
-		}
-	}
+  data() {
+    return {
+      time: "0:00",
+      running: false,
+      hidden: false,
+      text: 'UP NEXT',
+      newText: '',
+      nextTime: null
+    };
+  },
+  created() {
+    NodeCG.waitForReplicants(countdownRep).then(this.listen);
+  },
+  methods: {
+    toggleTimer() {
+      if (running.value) {
+        nodecg.sendMessage('stopCountdown', countdownNext.value);
+      } else {
+        nodecg.sendMessage('startCountdown', countdownNext.value);
+      }
+    },
+    toggleHidden() {
+      nodecg.sendMessage('showHideTimer');
+    },
+    setText() {
+      countdownText.value = this.newText;
+    },
+    listen() {
+      this.newText = countdownText.value;
+      countdownRep.on('change', newVal => {
+        this.time = newVal.formatted;
+        this.hidden = newVal.hidden;
+      });
+      running.on('change', newVal => {
+        this.running = newVal;
+      });
+      countdownText.on('change', newVal => {
+        this.text = newVal;
+      });	
+      countdownNext.on('change', newVal => {
+        let formattedNextTime = TimeUtils.createTimeStruct(newVal * 1000);
+        this.nextTime = formattedNextTime.formatted;
+      });
+    }
+  },
+  computed: {
+    startStopContent() {
+      return this.running ? 'STOP' : 'START';
+    },
+    startStopColour() {
+      return this.running ? 'red' : 'green';
+    },
+    showHideContent() {
+      return this.hidden ? 'SHOW' : 'HIDE';
+    },
+    showHideColour() {
+      return !this.hidden ? 'red' : 'green';
+    }
+  }
 };
 </script>
 
 <style lang="scss" scoped>
 .time {
-	font-size: 70px;
+  font-size: 70px;
 }
 .text {
-	width: 100%;
-	text-align: center;
+  width: 100%;
+  text-align: center;
 }
 </style>
