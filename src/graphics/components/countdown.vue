15e9ef2f5bfdd14c7ead24d7b638507579fc28a5
Joshua Fillingham <j.j.fillingham@edu.salford.ac.uk>
2019-03-02 03:03:28 +0000

    fix: starting scene countdown

--- src/graphics/components/countdown.vue
+++ src/graphics/components/countdown.vue
@@ -1,16 +1,38 @@
 <template>
-  <div class="countdown">
-    <h1 class='countdown'>{{countdown}}</h1>
+  <div>
+    <h1 class="somthingsensible">{{countdown}}</h1>
   </div>
 </template>
 
 <script>
+const countdownRep = nodecg.Replicant('countdown');
 export default {
-	
+	data() {
+		return {
+			countdown: "0:00",
+		};
+  },
+  
+	created() {
+		NodeCG.waitForReplicants(countdownRep).then(this.listen);
+  },
+
+  methods: {
+    listen() {
+      countdownRep.on('change', newVal => {
+        var reformattedtime = newVal.formatted.slice(0, newVal.formatted.length-2);
+        this.countdown = reformattedtime;
+      });
+    }
+  }
 }
 </script>
 
 <style lang="scss" scoped>
-
+@import '../../../shared/style/main.scss';
+.somthingsensible{
+  color: $primary;
+  font-size: 500px
+}
 </style>
 
