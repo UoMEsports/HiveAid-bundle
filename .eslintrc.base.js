31aff136423af4c853dc892a7903125c84ec9057
Dan Shields <d.shieldsuk@gmail.com>
2019-03-07 18:50:41 +0000

    chore: replace TSLint with ESLint to try to create some code consistency

---
+++ .eslintrc.base.js
@@ -0,0 +1,7 @@
+module.exports = {
+	root: true,
+	rules: {
+		"indent": ["error", 4],
+		"no-use-before-define": ["error", { "functions": false}]
+	}
+}
--- .eslintrc.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-	root: true,
-	extends: [
-	  'plugin:vue/recommended'
-	]
-}
