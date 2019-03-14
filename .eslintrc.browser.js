235e4637ad1c5d96eac969548db443c60374a17a
Dan Shields <d.shieldsuk@gmail.com>
2019-03-14 15:57:49 +0000

    chore(eslint): enforce no self-closing html vue components

--- .eslintrc.browser.js
+++ .eslintrc.browser.js
@@ -2,5 +2,12 @@ module.exports = {
 	extends: [
 		'plugin:vue/recommended',
 		'./.eslintrc.base.js'
-	]
+	],
+	rules: {
+		"vue/html-self-closing": ["error", {
+			"html": {
+				"component": "never"
+			}
+		}]
+	}
 }
