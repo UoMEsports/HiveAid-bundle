5617104360ba3c207d527a5a7bdc21f548f4c57d
Joshua Fillingham <j.j.fillingham@edu.salford.ac.uk>
2019-03-29 01:19:04 +0000

    feat: added donation popup graphic

---
+++ src/graphics/components/donation-popup.vue
@@ -0,0 +1,34 @@
+<template>
+    <h1>{{ donation }}</h1>
+</template>
+
+<script>
+const countdownRep = nodecg.Replicant('countdown');
+export default {
+    data() {
+        return {
+            donation: "",
+        };
+    },
+  
+    created() {
+        nodecg.listenFor('donation', this.newDonation);
+    },
+
+    methods: {
+        newDonation(data) {
+            var donation = data.name + " just donated " + data.formattedAmount;
+            this.donation = donation;
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+@import '../../../shared/style/main.scss';
+
+h1 {
+  color: $primary;
+  font-size: 500px
+}
+</style>
